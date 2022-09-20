33613937cbf5bbec32f8cc6628d0cc3439b8e49a
Celine MOREL A L'HUISSIER <cmorel-a@e1r6p4.clusters.42paris.fr>
2022-09-20 17:35:29 +0200

    Add function to split a string using a string delimiter

--- sources/utils.cpp
+++ sources/utils.cpp
@@ -9,3 +9,18 @@ void split_args(std::string in, char *delimiter, std::vector<std::string> &out)
         token = strtok(NULL, delimiter);
 	}
 }
+
+std::vector<std::string> split_on_string(std::string message, std::string delim)
+{
+	std::vector<std::string> out;
+    std::string::size_type start = 0U;
+    std::string::size_type end = message.find(delim);
+    while (end != std::string::npos)
+    {
+        out.push_back(message.substr(start, end - start));
+        start = end + delim.length();
+        end = message.find(delim, start);
+    }
+    out.push_back(message.substr(start, end));
+	return out;
+}
