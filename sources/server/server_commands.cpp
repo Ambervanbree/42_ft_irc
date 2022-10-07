b5665cf10fabfbda54bdcb98ba6f6a236722b0f8
Chleo PROESCHEL <cproesch@e3r10p9.42.fr>
2022-10-07 15:46:18 +0200

    change branch name

--- sources/server/server_commands.cpp
+++ sources/server/server_commands.cpp
@@ -6,8 +6,7 @@ void Server::_setCommands()
 	_commands["PASS"] = PASS;
 	_commands["USER"] = USER;
 	_commands["OPER"] = OPER;
-	// _commands["CONNECT"] = CONNECT;
-	// _commands["KILL"] = KILL;
+	_commands["KILL"] = KILL;
 	// _commands["PING"] = PING;
 	// _commands["PONG"] = PONG;
 	// _commands["ERROR"] = ERROR;			x
