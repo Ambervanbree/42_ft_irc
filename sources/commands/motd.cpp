e4281a7a88a755724d83b32baf249a64ce33e2f0
Amber van Bree <avan-bre@students.42.fr>
2022-10-11 16:06:25 +0200

    erased includes

--- sources/commands/motd.cpp
+++ sources/commands/motd.cpp
@@ -4,27 +4,19 @@
 #include "commands.hpp"
 #include <fstream>
 
-# define RESET   "\033[0m"
-# define GREEN	 "\033[32m"
-# define YELLOW  "\033[33m"
-# define BLUE    "\033[34m"
-# define CYAN    "\033[36m"
-# define RED     "\033[31m"
-# define MAGENTA "\033[35m"
-# define BOLD    "\e[1m"
-# define ITALIC	 "\e[3m"
-
 void	sendMOTD(User &user){
 	std::ifstream	MOTDfile;
 	std::string		line;
 
 	MOTDfile.open("other/motd.txt");
 	if (MOTDfile.is_open()){
+		// PRIVMSG to user: RPL_MOTDSTART (375)
 		while (MOTDfile.good()){
 			std::getline(MOTDfile, line);
 			std::cout << line << std::endl;
 			//PRIVMSG to user: line + "\n";
 		}
+		// PRIVMSG to user: RPL_ENDOFMOTD (376)
 		MOTDfile.close();
 	}
 	else
